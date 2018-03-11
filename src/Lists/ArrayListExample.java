8b340477080b0f77a94b6f7689cd5bd809ef56a8
dimulia83@yandex.com <DimaK@192.168.12.160>
2018-03-11 13:55:16 -0400

    initial commit

---
+++ src/Lists/ArrayListExample.java
@@ -0,0 +1,37 @@
+package Lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		List<String> l = new ArrayList<>();
+		l.add("water");
+		l.add("coffee");
+		l.add("wine");
+		l.add("tea");
+		l.add("milk");
+		System.out.println(l.indexOf("tea"));//	3
+		
+		String[] array = l.toArray(new String[l.size()]);
+		Arrays.sort(array);
+		System.out.println(Arrays.binarySearch(array, "wine"));//	4
+	
+		System.out.println(l);
+			l.clear();
+		int[][] nums = new int[2][3];
+		List<List<Integer>> list;//	nested List such multidimensional array
+		
+		int[] numbers = {1,4,7,8,5,2};
+		System.out.println(Arrays.toString(numbers));
+		numbers = Arrays.copyOf(numbers, numbers.length + 5);
+		System.out.println(Arrays.toString(numbers));
+		
+		
+		
+
+	}
+
+}
